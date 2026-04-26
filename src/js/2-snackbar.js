2eed35b2b528421d3007183fee154487b743878c
SuperCat2812 <64689542+SuperCat2812@users.noreply.github.com>
2026-04-26 12:10:43 +0200

    Refactor event handling in snackbar to use form submission instead of button click

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,41 +3,42 @@ const refs = {
   delay: document.querySelector('input[name="delay"]'),
   radioBtnTrue: document.querySelector('input[value="fulfilled"]'),
   radioBtnFalse: document.querySelector('input[value="rejected"]'),
-  button: document.querySelector('button'),
+  form: document.querySelector('.form'),
 };
 
-refs.button.addEventListener('click', e => {
+refs.form.addEventListener('submit', e => {
   e.preventDefault();
-  setTimeout(() => {
-    if (refs.delay.value && (refs.radioBtnTrue.checked || refs.radioBtnFalse.checked)) {
-      e.preventDefault();
-      const promise = new Promise((fulfilled, reject) => {
+
+  if (refs.delay.value && (refs.radioBtnTrue.checked || refs.radioBtnFalse.checked)) {
+    e.preventDefault();
+    const promise = new Promise((fulfilled, reject) => {
+      setTimeout(() => {
         if (refs.radioBtnTrue.checked) {
           fulfilled(refs.delay.value);
         } else {
           reject(refs.delay.value);
         }
-      });
+      }, refs.delay.value);
+    });
 
-      promise
-        .then(result => {
-          iziToast.show({
-            position: 'topRight',
-            backgroundColor: 'green',
-            title: `✅ Fulfilled promise in ${result}ms`,
-            titleColor: '#ffff',
-            titleSize: 16,
-          });
-        })
-        .catch(error => {
-          iziToast.show({
-            position: 'topRight',
-            backgroundColor: '#EF4040',
-            title: `❌ Rejected promise in ${error}ms`,
-            titleColor: '#ffff',
-            titleSize: 16,
-          });
+    promise
+      .then(result => {
+        iziToast.show({
+          position: 'topRight',
+          backgroundColor: 'green',
+          title: `✅ Fulfilled promise in ${result}ms`,
+          titleColor: '#ffff',
+          titleSize: 16,
         });
-    }
-  }, refs.delay.value);
+      })
+      .catch(error => {
+        iziToast.show({
+          position: 'topRight',
+          backgroundColor: '#EF4040',
+          title: `❌ Rejected promise in ${error}ms`,
+          titleColor: '#ffff',
+          titleSize: 16,
+        });
+      });
+  }
 });
